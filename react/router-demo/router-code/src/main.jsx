f44752bcc0b232a655ba6532d3162504b7faa6fe
hgb <3299848100@qq.com>
2022-06-21 11:07:04 +0800

    手写单页应用 但未解决捕获hashChange事件

--- react/router-demo/router-code/src/main.jsx
+++ react/router-demo/router-code/src/main.jsx
@@ -5,10 +5,10 @@ import './index.css'
 // 1. BrowserRouter? 路由组件 
 // raect-router-dom 路由组件库
 // 2. 可以给我们的 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
