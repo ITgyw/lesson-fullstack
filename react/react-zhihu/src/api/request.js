716f5012ee29010bea3300436e8cd4dc321cf828
hgb <3299848100@qq.com>
2022-06-26 23:27:00 +0800

    完成banners的轮播

--- react/react-zhihu/src/api/request.js
+++ react/react-zhihu/src/api/request.js
@@ -2,3 +2,5 @@ import axios from 'axios'
 
 export const getsubjectTitle = () =>
     axios.get('https://www.fastmock.site/mock/5c02d0c163ad71c2b1279dea641af126/mook/subjectTitle')
+export const getBanners = () =>
+    axios.get('https://www.fastmock.site/mock/5c02d0c163ad71c2b1279dea641af126/mook/banners')
--- react/react-zhihu/src/pages/Home/HomeRecommend/index.jsx
+++ react/react-zhihu/src/pages/Home/HomeRecommend/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Banners from './Banners'
+import { getBanners } from '@/api/request'
+
+
 // const backgroungImg = "@/assets/imgs/1.png"
 export default function HomeRecommend() {
+    const [banners, setBanners] = useState([])
+    useEffect(() => {
+        (async () => {
+            let { data: bannerData } = await getBanners()
+            setBanners(bannerData)
+        })()
+    }, [])
     return (
         <div>
             HomeRecommend
-            <img src="" alt="1" />
+            {/* <img src="https://fuss10.elemecdn.com/0/d0/dd7c960f08cdc756b1d3ad54978fdjpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/" alt="1" /> */}
+            <Banners banners={banners} />
         </div>
     )
 }
