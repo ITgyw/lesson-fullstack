d13e32b3c92e7c70fd081805a0dbc5a7c25a1ad9
hgb <3299848100@qq.com>
2022-07-19 16:30:27 +0800

    画圆

--- react/antd-admin-blog/src/pages/Login/LoginBg/index.jsx
+++ react/antd-admin-blog/src/pages/Login/LoginBg/index.jsx
@@ -6,7 +6,7 @@ export default function LoginBg() {
         const BACKGROUND_COLOR = 'rgb(0,45,54)';
         const POINT_NUM = 80; // 星星的数量
         const POINT_COLOR = 'rgb(255,255,255)'
-
+        const LINE_LENGTH = 10000;
         // 画
         // canvas 语义化标签，video audio  type=range color email requied  
         // html5  localStorage{一直存在}  kv  sessionStorage{窗口关闭时消失}
@@ -34,6 +34,32 @@ export default function LoginBg() {
             this.dy = Math.cos(angle) * speed;
             this.r = 1.2
             this.color = POINT_COLOR;
+        }
+        Point.prototype.move = function () {
+            this.x += this.dx;
+            if (this.x < 0) { // 左边的边界
+                this.x = 0;
+                this.dx = -this.dx; // 反向
+            } else if (this.x > cvs.width) {
+                this.x = cvs.width;
+                this.dx = -this.dx;
+            }
+            thiis.y += this.dy;
+            if (this.y < 0) {
+                this.y = 0;
+                this.dy = -this.dy;
+            } else if (this.y > cvs.height) {
+                this.y = cvs.height;
+                this.dy = -this.dy;
+            }
+        }
+
+        Point.prototype.draw = function () {
+            // canvas html5 绘图
+            ctx.fillStyle = this.color; //什么颜色的画笔
+            ctx.beginPath(); // 表示开始画
+            // 画圆 
+            ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
         }
     }, [])
     return (
