0a57c5badcccb243624f39072e8df39aa3ec1dc9
hgb <3299848100@qq.com>
2022-07-19 16:49:25 +0800

    背景

--- react/antd-admin-blog/src/pages/Login/LoginBg/index.jsx
+++ react/antd-admin-blog/src/pages/Login/LoginBg/index.jsx
@@ -3,48 +3,50 @@ import './index.css'
 
 export default function LoginBg() {
     useEffect(() => {
-        const BACKGROUND_COLOR = 'rgb(0,45,54)';
-        const POINT_NUM = 80; // 星星的数量
-        const POINT_COLOR = 'rgb(255,255,255)'
+        // 常量
+        const BACKGROUND_COLOR = 'rgb(0, 43, 54)';
+        const POINT_NUM = 80; // 星星数量
+        const POINT_COLOR = 'rgb(255, 255, 255)';
         const LINE_LENGTH = 10000;
         // 画
-        // canvas 语义化标签，video audio  type=range color email requied  
-        // html5  localStorage{一直存在}  kv  sessionStorage{窗口关闭时消失}
-        // cookie 是无脑携带 每次都携带 所以http还是无状态
-        // session 会话 完全全在后端 内存来保存的
-
+        // canvas 语义化, video audio type=range color email required
+        // html5  localStorage kv sessionStorage  
+        // session 会话 后端 内存 
         // canvas 与其他的html 标签不一样 api 绘图
-        const cvs = document.createElement('canvas') // 画布
+        const cvs = document.createElement('canvas'); // 画布
         cvs.width = window.innerWidth;
         cvs.height = window.innerHeight;
-        cvs.style.cssText = 'position:fixed;top:0px;left:0px;z-index:-1;opacity:1.0';
+        cvs.style.cssText =
+            'position:fixed;top:0px;left:0px;z-index:-1;opacity:1.0;';
         document.body.appendChild(cvs);
-        const ctx = cvs.getContext('2d');// 取得 上下文
+        const ctx = cvs.getContext('2d'); // 上下文 
 
         function randomFloat(min, max) {
-            return min + Match.random() * (max - min)
+            return min + Math.random() * (max - min)
         }
+
         function Point() {
-            this.x = randomFloat(0, cvs.width)
-            this.y = randomFloat(0, cvs.height)
-            const speed = randomFloat(0.3, 1.4)
-            const angle = randomFloat(0, 2 * Math.Pi);
-            // dx 
+            this.x = randomFloat(0, cvs.width);
+            this.y = randomFloat(0, cvs.height);
+            const speed = randomFloat(0.3, 1.4);
+            const angle = randomFloat(0, 2 * Math.PI);
+            // 
             this.dx = Math.sin(angle) * speed;
             this.dy = Math.cos(angle) * speed;
             this.r = 1.2
             this.color = POINT_COLOR;
         }
+
         Point.prototype.move = function () {
             this.x += this.dx;
-            if (this.x < 0) { // 左边的边界
-                this.x = 0;
-                this.dx = -this.dx; // 反向
-            } else if (this.x > cvs.width) {
+            if (this.x < 0) { // 左边的边界 
+                this.x = 0; //  不离开
+                this.dx = -this.dx; // 反向 
+            } else if (this.x > cvs.width) { // 右边的边界
                 this.x = cvs.width;
                 this.dx = -this.dx;
             }
-            thiis.y += this.dy;
+            this.y += this.dy;
             if (this.y < 0) {
                 this.y = 0;
                 this.dy = -this.dy;
@@ -55,16 +57,52 @@ export default function LoginBg() {
         }
 
         Point.prototype.draw = function () {
-            // canvas html5 绘图
-            ctx.fillStyle = this.color; //什么颜色的画笔
-            ctx.beginPath(); // 表示开始画
+            // canvas html5 绘图 
+            ctx.fillStyle = this.color;// 什么颜色的画笔
+            ctx.beginPath(); // 开始画
             // 画圆 
             ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+            ctx.closePath();// 结束绘制
+            ctx.fill();
         }
+
+        const points = [];
+        function initPoints(num) {
+            for (let i = 0; i < num; ++i) {
+                points.push(new Point());
+            }
+        }
+
+        const p0 = new Point(); // 鼠标跟随的小星星
+        p0.dx = p0.dy = 0;
+        document.onmousemove = function (ev) {
+            p0.x = ev.clientX;
+            p0.y = ev.clientY;
+        }
+        document.onmousedown = function (ev) {
+            p0.x = ev.clientX;
+            p0.y = ev.clientY;
+        }
+        document.onmouseup = function (ev) {
+            p0.x = ev.clientX;
+            p0.y = ev.clientY;
+        }
+        document.onmouseout = function (ev) {
+            p0.x = null;
+            p0.y = null;
+        }
+
+        function drawFrame() {
+            cvs.width = window.innerWidth;
+            cvs.height = window.innerHeight;
+            ctx.fillStyle = BACKGROUND_COLOR;
+            ctx.fillRect(0, 0, cvs.width, cvs.height);
+        }
+
+        initPoints(POINT_NUM)
+        drawFrame();
     }, [])
     return (
-        <>
-            LoginBg
-        </>
+        <></>
     )
 }
