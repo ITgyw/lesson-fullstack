2eee4495cfa0eec45a0efd8f03a85a3c974b9ab5
hgb <3299848100@qq.com>
2022-07-29 23:32:47 +0800

    Home

--- react/bilibili-ssp-fe/src/pages/Home/index.tsx
+++ react/bilibili-ssp-fe/src/pages/Home/index.tsx
@@ -1,16 +1,73 @@
 import React, { useEffect } from 'react'
 import Header from '@/components/Header'
 import { getPartitionsRequest } from '@/api/request'
+import { connect } from 'react-redux'
+import { rootState } from '@/store'
+import { Dispatch } from 'redux'
+import { getHomeDataAction } from '@/store/actionCreators'
+import { HomeWrapper } from './style'
+import { PartitionType } from '@/models/index'
+import TabBar from '@/components/TabBar'
 
-const Home = () => {
-    useEffect(() => {
-        getPartitionsRequest()
-    }, [])
-    return (
-        <div>
-            <Header />
-        </div>
-    )
+interface HomeProps {
+  loading: boolean;
+  rankingPartitions: any[];
+  rankingVideos: any[];
+  oneLevelPartitions: any[];
+  getHomeDataActionDispatch: (rId: string) => void;
 }
 
-export default Home
+const Home: React.FC<HomeProps> = (props) => {
+  const {
+    loading,
+    rankingPartitions,
+    rankingVideos,
+    oneLevelPartitions
+  } = props
+  const {
+    getHomeDataActionDispatch
+  } = props
+  useEffect(() => {
+    getHomeDataActionDispatch("1");
+  }, [])
+  const handleClick = () => {
+
+  }
+
+  const tabBarData: PartitionType[] = [{ id: 0, name: "首页" }]
+    .concat(oneLevelPartitions);
+  tabBarData.push({ id: -1, name: "直播" })
+
+  return (
+    <HomeWrapper className="top-wrapper">
+      <Header />
+      <div className="partition">
+        <div className="tab-bar">
+          <TabBar
+            data={tabBarData}
+            type="indicate"
+            onClick={handleClick}
+          />
+        </div>
+        <div className="switch">
+          <i className="icon-arrow-down"></i>
+        </div>
+      </div>
+    </HomeWrapper>
+  )
+}
+
+const mapStateToProps = (state: rootState) => ({
+  loading: state.loading,
+  rankingPartitions: state.ranking.rankingPartitions,
+  rankingVideos: state.ranking.rankingVideos, 7
+  oneLevelPartitions: state.partitions.oneLevelPartitions
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getHomeDataActionDispatch(rId: string) {
+    dispatch(getHomeDataAction(rId))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
