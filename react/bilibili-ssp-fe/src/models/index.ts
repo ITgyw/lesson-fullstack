136bca3c645c6ebab8ab2fea680ff5707dd9c6e9
hgb <3299848100@qq.com>
2022-07-29 23:33:32 +0800

    添加interface

--- react/bilibili-ssp-fe/src/models/index.ts
+++ react/bilibili-ssp-fe/src/models/index.ts
@@ -1,4 +1,4 @@
 export {
     PartitionType,
     createPartitionTypes
-} from "./partitionType";
+} from "./PartitionType";
--- react/bilibili-ssp-fe/src/models/partitionType.ts
+++ react/bilibili-ssp-fe/src/models/partitionType.ts
@@ -1,16 +1,9 @@
-/**
- * 分类
- */
-class PartitionType {
-    constructor(public id: number, public name: string,
-        public children: PartitionType[] = []) { }
-}
-function createPartitionTypes(data: any[]): PartitionType[] {
-    return data.map((item) => new PartitionType(item.tid, item.typename));
+interface PartitionType {
+    id: number;
+    name: string;
+    children?: PartitionType[]
 }
 
-export {
-    PartitionType,
-    createPartitionTypes
-};
-
+export type {
+    PartitionType
+}
