310a09730d88bc0c99f77228ecf83d472b2dcb66
hgb <3299848100@qq.com>
2022-07-29 17:04:30 +0800

    store

--- react/bilibili-ssp-fe/src/store/index.ts
+++ react/bilibili-ssp-fe/src/store/index.ts
@@ -1,10 +1,10 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware  } from 'redux';
 import reducers from './reducers';
 import thunk, { ThunkMiddleware } from "redux-thunk";
 
-const composeEnhancers =
+const composeEnhancers = 
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,
+const store = createStore(reducers, 
     composeEnhancers(applyMiddleware(thunk as ThunkMiddleware)));
 // Infer the `RootState` and `AppDispatch` types from the store itself
 
