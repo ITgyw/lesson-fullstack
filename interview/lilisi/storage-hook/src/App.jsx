be6567c9f309eb654f78081d4d113f1ebdff6a8e
hgb <3299848100@qq.com>
2022-09-13 00:12:21 +0800

    useRef.current 实现存值的功能

--- interview/lilisi/storage-hook/src/App.jsx
+++ interview/lilisi/storage-hook/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import useLoacalStorageState from './useLocalStorageState';
 
 function App() {
+  const ref = useRef();
   const STORAGE_NAME = 'theme'
   const Address_NAME = 'address'
   const defaultValue = {
@@ -14,6 +15,14 @@ function App() {
   // })
   const [value, setValue] = useLoacalStorageState(STORAGE_NAME, defaultValue.theme)
   const [address, setAddress] = useLoacalStorageState(Address_NAME, defaultValue.address)
+  const [testA, setTestA] = useState('a')
+  // 未设置[] 挂载时运行 更新时运行
+  useEffect(() => {
+    setTestA('b');
+    ref.current = 'b';
+    console.log(testA, ref.current);
+    // console.log(testA);
+  }, [])
   // const changeItem = (value) => {
   //   setValue(value)
   //   // localStorage.setItem(STORAGE_NAME, v6rdsaalue)
@@ -21,6 +30,7 @@ function App() {
 
   return (
     <div className="App">
+      {testA}
       <input value={value} onChange={e => setValue(e.target.value)} />
       <input value={address} onChange={e => setAddress(e.target.value)} />
     </div>
